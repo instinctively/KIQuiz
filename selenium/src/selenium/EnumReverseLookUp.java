0249ce002a1541edbb1cbc8116404e356f7d0a6b
instinctively <deviouslyinstinctious@gmail.com>
2016-12-20 23:54:11 +0530

    Delete EnumReverseLookUp.java

--- selenium/src/selenium/EnumReverseLookUp.java
+++
@@ -1,9 +0,0 @@
-package selenium;
-import quiz.AmericanPresidents;
-
-public class EnumReverseLookUp extends AmericanPresidents {
-	public static void main(String[] args) {
-		AmericanPresidents americanPresidents = new AmericanPresidents();
-		System.out.print(americanPresidents.getAnswer("Who was the 37th president of the United States?"));
-	}
-}
